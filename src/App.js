9a75bc6eeb5475c87c62a51a68f514288c2a4164
Jesse Young <young295@purdue.edu>
2021-04-16 17:43:10 -0400

    app can now handle image drop with error checks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import './App.scss';
 import logo from './images/my-palette-logo.png'
 
-function App() {
+//fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFileUpload } from '@fortawesome/free-solid-svg-icons'
+
+import Upload from './upload/Upload'
+
+
+library.add(faFileUpload)
+
+const App = () => {
+
+  const collectFile = (file) => {
+    console.log(file)
+  }
+  
   return (
-    <div className="container">
-      <div className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="/#">
-            <img alt="my palette logo" src={logo}/>
-            <p className="logo">&nbsp;My Palette</p>
-          </a>
+    <>
+      <div className="container">
+        <div className="navbar" role="navigation" aria-label="main navigation">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/#">
+              <img alt="my palette logo" src={logo}/>
+              <p className="logo">&nbsp;My Palette</p>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="section is-medium">
+        <div className="container">
+          <Upload collectFile={collectFile}/>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/upload/Upload.js
@@ -0,0 +1,55 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react'
+import './Upload.scss';
+
+const MAX_FILE_SIZE_BYTES = 5000000
+
+const Upload = ({
+    collectFile,
+    maxFileSize = MAX_FILE_SIZE_BYTES
+}) => {
+
+    const [hasSizeError, setHasSizeError] = useState(false);
+    const [hasTypeError, setHasTypeError] = useState(false);
+
+    const handleFileUpload = (e) => {
+        setHasSizeError(false)
+        setHasTypeError(false)
+
+        const newFile = e.target.files[0]
+
+        if (newFile.size > maxFileSize) {
+            setHasSizeError(true)
+        } else if (newFile.type !== "image/jpeg" && newFile.type !== "image/png") {
+            setHasTypeError(true)
+        } else {
+            setHasSizeError(false)
+            setHasTypeError(false)
+            collectFile(newFile)
+        }
+    }
+
+    return (
+        <section className='file-uploader content box'>
+            <FontAwesomeIcon icon="file-upload" size="5x" />
+
+            {hasSizeError && (
+                <p className="hasError">Your photo was too large! Please choose another under 5mb.</p>
+            )}
+
+            {hasTypeError && (
+                <p className="hasError">Please upload only JPEG or PNG files.</p>
+            )}
+
+            {!hasSizeError && !hasTypeError && (
+                <p>Drop your photo here, or <strong>Browse</strong></p>
+            )}
+            
+            <span>Supports JPG, PNG. Max File Size: 5mb</span>
+            <input className="file-uploader__input" type="file" accept="image/png, image/jpeg" onChange={handleFileUpload}/>
+        </section>
+    )
+}
+
+
+export default Upload
