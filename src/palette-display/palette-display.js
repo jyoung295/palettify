3ecb9fca0075a02c9e82e4456304cacf3c1dadc5
Jesse Young <young295@purdue.edu>
2021-07-23 16:36:20 -0400

    add click to copy feature

--- src/palette-display/palette-display.js
+++ src/palette-display/palette-display.js
@@ -9,6 +9,11 @@ const PaletteDisplay = ({
     const imgSrc = paletteData.paletteCanvas
     const colors = paletteData.paletteColorsArray
 
+    const copyText = (e) => {
+        const text = e.target.innerText
+        navigator.clipboard.writeText(text)
+    }
+
     return (
         <div className="palette-display">
             <img className="palette-display--image" alt="Your original upload with your palette!" src={imgSrc} />
@@ -29,9 +34,9 @@ const PaletteDisplay = ({
                                 </td>
                                 {/* right column, color codes */}
                                 <td className="color-codes">
-                                    <p className="color-codes--rgb"><strong>RGB:</strong> {`${color.rgb.r} ${color.rgb.g} ${color.rgb.b}`}</p>
-                                    <p className="color-codes--hex"><strong>HEX:</strong> {`#${color.hex}`}</p>
-                                    <p className="color-codes--cmyk"><strong>CMYK:</strong> {`${color.cmyk[0]} ${color.cmyk[1]} ${color.cmyk[2]} ${color.cmyk[3]}`}</p>
+                                    <p className="color-codes--rgb"><strong>RGB:</strong> <button onClick={copyText} className="color-codes--copy-button">{`${color.rgb.r} ${color.rgb.g} ${color.rgb.b}`}</button> </p>
+                                    <p className="color-codes--hex"><strong>HEX:</strong> <button onClick={copyText} className="color-codes--copy-button">{`#${color.hex}`}</button> </p>
+                                    <p className="color-codes--cmyk"><strong>CMYK:</strong> <button onClick={copyText} className="color-codes--copy-button">{`${color.cmyk[0]} ${color.cmyk[1]} ${color.cmyk[2]} ${color.cmyk[3]}`}</button> </p>
                                 </td>
                             </tr>
                         )
